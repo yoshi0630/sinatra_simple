02ee447b1ac8397cbb427e19d84914034d34c262
yossi <yossi@debian>
2015-04-04 11:53:06 +0900

    fixed main

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 require 'sinatra'
 require 'sqlite3'
-require 'pp'
-
+# require 'pp'
+require 'securerandom'
 
 
 db = SQLite3::Database.new "db/post.db"
@@ -16,7 +16,7 @@ get '/' do
   erb :index,{ :locals => { :posts => posts} }
 end
 
-get '/hello' do
+get '/hello' 
   "Hello world"
 end
 
@@ -28,8 +28,30 @@ post '/' do
 
 # p params["ex_text"]
 
-stmt = db.prepare("INSERT INTO posts (text) VALUES (?)")
-  stmt.bind_params(params["ex_text"])
+# stmt = db.prepare("INSERT INTO posts (text) VALUES (?)")
+  # stmt.bind_params(params["ex_text"])
+file_name = ""
+
+if params["file"]
+ext = ""
+if params["file"][:type].include? "jpeg"
+ext = "jpg"
+elsif params["file"][:type].include? "png"
+ext = "png"
+else
+return "投稿できる画像形式はjpgとpngだけです"
+end
+
+# 適当なファイル名を付ける
+file_name = SecureRandom.hex + "." + ext
+
+# 画像を保存
+File.open("./public/uploads/" + file_name, 'wb') do |f|
+f.write params["file"][:tempfile].read
+end
+
+stmt = db.preapare("INSERT INTO posts (text, img_file_name VALUES (?, ?")
+stmt.bind_params(params["ex_text"], file_name)
   stmt.execute
   redirect '/'
 end
