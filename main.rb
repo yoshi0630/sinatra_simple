f7e2e1e071f58a0394724f01e7aefc6b4506f3a7
yossi <yossi@debian>
2015-04-04 10:55:39 +0900

    fixed main

--- main.rb
+++ main.rb
@@ -1,4 +1,9 @@
 require 'sinatra'
+require 'sqlite3'
+
+db = SQLite3::Database.new "db/post.db"
+db.results_as_hash = true
+
 
 get '/' do
   erb :index
@@ -14,6 +19,10 @@ end
 
 post '/' do
 
-p params["ex_text"]
+# p params["ex_text"]
 
+stmt = db.prepare("INSERT INTO posts (text) VALUES (?)")
+  stmt.bind_params(params["ex_text"])
+  stmt.execute
+  redirect '/'
 end
