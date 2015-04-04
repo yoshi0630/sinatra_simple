78e22141680a6cd602057a6db95924e9a7ce3c90
yossi <yossi@debian>
2015-04-04 11:23:38 +0900

    standbye ok

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
+# encoding: utf-8
 require 'sinatra'
 require 'sqlite3'
 require 'pp'
+
 
 
 db = SQLite3::Database.new "db/post.db"
@@ -11,7 +13,7 @@ get '/' do
   posts = db.execute("SELECT * FROM posts ORDER BY id DESC")
   # pp posts
   # erb :index
-  erb :index,{ :locals => {:posts => { :posts => posts} }
+  erb :index,{ :locals => { :posts => posts} }
 end
 
 get '/hello' do
