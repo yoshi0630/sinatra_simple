22bdd7d5b5aa16129d11c2c6cab6cfe1abea1630
yossi <yossi@debian>
2015-04-18 13:55:13 +0900

    a lot of error

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 require 'sinatra'
 require 'sqlite3'
-# require 'pp'
 require 'securerandom'
+require 'sinatra/json'
 
 
 db = SQLite3::Database.new "db/post.db"
@@ -11,8 +11,6 @@ db.results_as_hash = true
 
 get '/' do
   posts = db.execute("SELECT * FROM posts ORDER BY id DESC")
-  # pp posts
-  # erb :index
   erb :index,{ :locals => { :posts => posts} }
 end
 
@@ -26,10 +24,6 @@ end
 
 post '/' do
 
-# p params["ex_text"]
-
-# stmt = db.prepare("INSERT INTO posts (text) VALUES (?)")
-  # stmt.bind_params(params["ex_text"])
 file_name = ""
 
 if params["file"]
@@ -50,14 +44,14 @@ File.open("./public/uploads/" + file_name, 'wb') do |f|
 f.write params["file"][:tempfile].read
 end
 
-stmt = db.preapare("INSERT INTO posts (text, img_file_name VALUES (?, ?")
+stmt = db.preapare("INSERT INTO posts (text, img_file_name VALUES (?, ?)")
 stmt.bind_params(params["ex_text"], file_name)
   stmt.execute
   redirect '/'
 end
 
 get '/star' do
-post_id = params["post_id"].to_1
+post_id = params["post_id"].to_i
 post = db.execute("SELECT star_count FROM posts
 WHERE id = ?", post_id)
 if post.empty?
@@ -68,5 +62,7 @@ new_star_count = post[0]["star_count"] + 1
 stmt = db.prepare("UPDATE posts SET star_count = ? WHERE id = ?")
 stmt.bind_params(new_star_count, post_id)
 stmt.execute
-return "スターを付けました"
+
+response = { "star_count" => new_star_count }
+json response
 end
